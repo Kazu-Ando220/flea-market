fb99686879588cb792404d78fed4b19e16eaeef7
k ando <umanokami@jcom.zaq.ne.jp>
2026-02-15 23:53:48 +0900

    ルーティング設定・初期ダミーデータ投入

--- database/seeders/ConditionsTableSeeder.php
+++ database/seeders/ConditionsTableSeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ConditionsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $conditions = [
+            ['id' => 1, 'content' => '良好'],
+            ['id' => 2, 'content' => '目立った傷や汚れなし'],
+            ['id' => 3, 'content' => 'やや傷や汚れあり'],
+            ['id' => 4, 'content' => '状態が悪い'],
+        ];
+
+        foreach($conditions as $condition) {
+            DB::table('conditions')->insert($condition);
+        }
     }
 }
